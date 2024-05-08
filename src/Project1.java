6cb68a8f60cf027f288f2e13f5a0623474011d22
Johnpaul Nguyen <146412036+Johnpaul-Nguyen@users.noreply.github.com>
2024-05-08 12:33:20 -0500

    Add files via upload

---
+++ src/Project1.java
@@ -0,0 +1,14 @@
+/**
+ * This is my first Java program.
+ * @author Johnpaul Nguyen
+ * @version 1.0
+ */
+public class Project1 {
+	// This is the main method. The instructions inside
+	// the braces are executed when the program runs.
+	public static void main(String[] args)
+	{
+		System.out.println("what do you call 8 rabbits? "
+				+ " A rabbyte!");
+	}
+}
